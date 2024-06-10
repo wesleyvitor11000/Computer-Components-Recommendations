9a4eae261065b3744f02a56ccd10275a5f28c16d
wesleyvitor11000 <wesleyvitor37417@gmail.com>
2024-06-10 00:29:41 -0300

    resolvind conflicts

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 # Adiciona o diretório pai ao sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-<<<<<<< HEAD
 from graphs.CompatibilityGraphGenerator import CompatibilityGraphGenerator, compatibility_constraints
 from graphs import GraphsUtil
 from components.Component import Component, GPU, ComponentTypes as ct
@@ -44,37 +43,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-=======
-from graphs.CompatibilityGraph import CompatibilityGraph, compatibility_constraints
-from components.Component import Component, ComponentTypes as ct
-from components.components_loading import import_components
-from random import Random
-
-
-def main():
-    
-    components = import_components(files_variants={
-        ct.MOTHERBOARD: "motherboards", ct.SSD: "ssds"
-    })
-    rd = Random()
-    
-    # rd.shuffle(components[ct.MOTHERBOARD])
-    # rd.shuffle(components[ct.CPU])
-    # rd.shuffle(components[ct.RAM])
-    
-    components[ct.MOTHERBOARD] = components[ct.MOTHERBOARD][:200:20]
-    components[ct.SSD] = components[ct.SSD][:700:50]
-    # components[ct.CPU] = components[ct.CPU][:200:20]
-    # components[ct.RAM] = components[ct.RAM][:200:20]
-    
-    
-    
-    CompatibilityGraph(
-        components,
-        compatibility_constraints
-    )
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> 7d96dbbfbc4c303d1624387f1b28394699a94866
